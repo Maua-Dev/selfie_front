03506f5bff06cac0abf58b5b0f53e511402f1df7
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-10-12 19:32:42 -0300

    fiz o retangulo da situacao do aluno
    Fiz o rentagulo da situação do aluno e montei um protótipo de página pra ver como ficaria a posição das coisas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { VisualizarFotoAlunoComponent } from './pages/visualizar-foto-aluno/visualizar-foto-aluno.component';
 import { StudentCardComponent } from './components/student-card/student-card.component';
+import { SituationRectangleComponent } from './components/situation-rectangle/situation-rectangle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VisualizarFotoAlunoComponent,
-    StudentCardComponent
+    StudentCardComponent,
+    SituationRectangleComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/situation-rectangle/situation-rectangle.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Student } from 'src/entities/student';
+
+@Component({
+  selector: 'app-situation-rectangle',
+  templateUrl: './situation-rectangle.component.html',
+  styleUrls: ['./situation-rectangle.component.css']
+})
+export class SituationRectangleComponent implements OnInit {
+
+  @Input() public studentToDisplay! : Student
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
