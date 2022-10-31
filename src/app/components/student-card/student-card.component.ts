9fbbbe7f4d1f12e2166c0432053b169dd475d684
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-10-31 19:29:24 -0300

    Botoes de aprovado e recusado usam a api para alterar o estado da selfie

--- src/app/components/student-card/student-card.component.ts
+++ src/app/components/student-card/student-card.component.ts
@@ -1,19 +1,27 @@
 import { Selfie } from 'src/entities/selfie';
 import { AfterContentInit, Component, Input, OnInit } from '@angular/core';
 import { Student } from 'src/entities/student';
+import { UpdateSelfieStateService } from 'src/app/services/update-selfie-state.service';
 
 @Component({
   selector: 'app-student-card',
   templateUrl: './student-card.component.html',
   styleUrls: ['./student-card.component.css']
 })
-export class StudentCardComponent implements AfterContentInit {
+export class StudentCardComponent implements OnInit {
 
   @Input() public studentToDisplay! : Student
   @Input() public photoToDisplay! : Selfie
-  constructor() { 
-  }
-  ngAfterContentInit(): void {
+  constructor(private updateSelfieService : UpdateSelfieStateService) { 
   }
 
+  SetSelfieStateAsReproved(newState : string) : void{
+    this.updateSelfieService.UpdateSelfieState(this.studentToDisplay.GetRA(), this.photoToDisplay.idSelfie.toString(), newState, '', '')
+    console.log(`Settei como ${newState}`)
+  }
+
+  ngOnInit(): void {
+
+  }
+ 
 }
