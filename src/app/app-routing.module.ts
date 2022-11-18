28574e4c826f9fe4d6a0417cf1ed03eb4313f9d4
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-11-18 15:40:07 -0300

    Coloquei a hist do aluno em outro modulo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { VisualizarFotoAlunoComponent } from './pages/tela-admin-ver-fotos/visualizar-foto-aluno.component';
-import { CommonModule } from '@angular/common';
-import { TelaTirarSelfieComponent } from './components/tela-tirar-selfie/tela-tirar-selfie.component';
-import { HomePageComponent } from './pages/tela-aluno/home-page/home-page.component';
 
 const routes: Routes = [
   {
@@ -13,8 +9,14 @@ const routes: Routes = [
         (m) => m.VisualizarFotoAlunoModule
       ),
   },
-  { path: 'aluno', component: HomePageComponent },
-  { path: 'selfie', component: TelaTirarSelfieComponent },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/tela-aluno/tela-aluno.module').then(
+        (m) => m.TelaAlunoModule
+      ),
+  },
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,14 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import { MatDialogModule } from '@angular/material/dialog';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { ProfileComponentComponent } from './components/profile-component/profile-component.component';
-import { BackgroundComponent } from './components/background/background.component';
-import { CardSelfieComponent } from './components/card-selfie/card-selfie.component';
-import { TituloPaginaComponent } from './components/titulo-pagina/titulo-pagina.component';
-import { CardStatusComponent } from './components/card-status/card-status.component';
-import { BackgroundGreyComponent } from './components/background-grey/background-grey.component';
-import { PopupComponent } from './components/popup/popup.component';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { TelaTirarSelfieComponent } from './components/tela-tirar-selfie/tela-tirar-selfie.component';
-import { HomePageComponent } from './pages/tela-aluno/home-page/home-page.component';
-import { CardSelfieTela2Component } from './components/card-selfie-tela2/card-selfie-tela2.component';
-import { WebcamModule } from 'ngx-webcam';
-import { SelfieStudent } from './services/selfie-student.service';
-import { SelfieStudentMockService } from './services/selfie-student-mock.service';
-import { CardStatusInicialComponent } from './components/card-status-inicial/card-status-inicial.component';
-import { PopupAtivarCameraComponent } from './components/popup-ativar-camera/popup-ativar-camera.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-
-    ProfileComponentComponent,
-    BackgroundComponent,
-    CardSelfieComponent,
-    TituloPaginaComponent,
-    CardStatusComponent,
-    BackgroundGreyComponent,
-    PopupComponent,
-    TelaTirarSelfieComponent,
-    HomePageComponent,
-    CardSelfieTela2Component,
-    CardStatusInicialComponent,
-    PopupAtivarCameraComponent,
   ],
   imports: [
     BrowserModule,
@@ -47,13 +16,9 @@ import { PopupAtivarCameraComponent } from './components/popup-ativar-camera/pop
     HttpClientModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-
-    NoopAnimationsModule,
-    MatDialogModule,
-    MatCheckboxModule,
-    WebcamModule,
+    NoopAnimationsModule
   ],
-  providers: [{ provide: SelfieStudent, useClass: SelfieStudentMockService }], //injecao de dependencia
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
