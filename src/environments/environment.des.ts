5841d7d9c534d723c06d33b32867081462075985
Hector Guerrini Herrera <hectorumaga@hotmail.com>
2022-11-23 21:51:19 -0300

    envs

--- src/environments/environment.des.ts
+++ src/environments/environment.des.ts
@@ -1,10 +1,8 @@
-const _BASE_URL_ADM_DOMAIN =
-  'https://vi7brr3n86.execute-api.us-east-1.amazonaws.com/prod/mss-student';
-const _BASE_URL_ESTUDANTE_DOMAIN =
-  'https://vi7brr3n86.execute-api.us-east-1.amazonaws.com/prod/mss-student';
+const _BASE_URL_ADM_DOMAIN = process.env["BASE_URL_ADM_DOMAIN"];
+const _BASE_URL_ESTUDANTE_DOMAIN = process.env["BASE_URL_ESTUDANTE_DOMAIN"];
 
 export const environment = {
   production: false,
   BASE_URL_ADM_DOMAIN: _BASE_URL_ADM_DOMAIN,
-  BASE_URL_ESTUDANTE_DOMAIN: _BASE_URL_ESTUDANTE_DOMAIN
+  BASE_URL_ESTUDANTE_DOMAIN:_BASE_URL_ESTUDANTE_DOMAIN
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,8 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-const _BASE_URL_ADM_DOMAIN =
-  'https://vi7brr3n86.execute-api.us-east-1.amazonaws.com/prod/mss-student';
-const _BASE_URL_ESTUDANTE_DOMAIN =
-  'https://vi7brr3n86.execute-api.us-east-1.amazonaws.com/prod/mss-student';
+const _BASE_URL_ADM_DOMAIN = process.env["BASE_URL_ADM_DOMAIN"];
+const _BASE_URL_ESTUDANTE_DOMAIN = process.env["BASE_URL_ESTUDANTE_DOMAIN"];
 
 export const environment = {
   production: false,
