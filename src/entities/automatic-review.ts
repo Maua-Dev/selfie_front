57b3a29b5890bb8da0a212b34f3e2c8d9e91d033
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-12-03 19:10:57 -0300

    fiz a lista de labels pre rejeitadas

--- src/entities/automatic-review.ts
+++ src/entities/automatic-review.ts
@@ -1,8 +1,38 @@
-export class AutomaticReview{
-    
-    constructor(public automaticallyRejected: boolean,
-        public rejectionReasons: string,
-        public labelsList?: string,
-    ){}
+export class AutomaticReview {
+  constructor(
+    public automaticallyRejected: boolean,
+    public rejectionReasons: string,
+    public labelsList?: any[]
+  ) {}
 
+  static RejectedLabelslist: string[] = [
+    'Hat',
+    'Cap',
+    'Glasses',
+    'Scarf',
+    'Electronics',
+    'Sunglasses',
+    'Finger',
+    'Pool',
+    'Nature',
+    'Vegetation',
+    'Restaurand',
+    'Furniture',
+    'Library',
+    'Airplane',
+    'Thophy',
+    'Bag',
+    'Shorts',
+    'Building',
+    'Architecture',
+    'Plant',
+    'Outdoors',
+    'Urban',
+    'City',
+    'Animal',
+    'Pet',
+    'Food',
+    'Electronics',
+    'Phone',
+  ];
 }
