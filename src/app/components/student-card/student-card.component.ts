1f4ec32756529a701a6590e4b0ac82b16965616a
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-12-19 14:54:48 -0300

    refatoracao de get first name para melhor legibilidade

--- src/app/components/student-card/student-card.component.ts
+++ src/app/components/student-card/student-card.component.ts
@@ -13,130 +13,93 @@ export class StudentCardComponent implements OnInit {
   @Input() public selfieToDisplay!: Selfie;
   studentEntity!: Student;
 
-  @Output() OnSetSelfieStateEvent: EventEmitter<any> = new EventEmitter(); 
+  @Output() OnSetSelfieStateEvent: EventEmitter<any> = new EventEmitter();
 
   ngOnInit(): void {
     this.studentEntity = this.selfieToDisplay.student!;
   }
 
-  GetStudentFirstName(): string{
-    let nameArray = this.studentEntity.name.split(" ")
+  GetStudentFirstName(): string {
+    let nameArray = this.studentEntity.name.split(' ');
 
+    if (nameArray.length > 1) {
+      const secondNameLowerCase = nameArray[1].toLocaleLowerCase();
+      const secondNamesMask = ['a', 'de', 'o', 'e', 'em'];
 
-    if(nameArray.length > 1){
-      if(nameArray[1].toLocaleLowerCase() == "de" || nameArray[1].toLocaleLowerCase() == "dos"|| nameArray[1].toLocaleLowerCase() == "e")
-        return `${nameArray[0]} ${nameArray[1]} ${nameArray[2]}`
+      if (secondNamesMask.includes(secondNameLowerCase))
+        return `${nameArray[0]} ${nameArray[1]} ${nameArray[2]}`;
 
-      return `${nameArray[0]} ${nameArray[1]}`
+      return `${nameArray[0]} ${nameArray[1]}`;
     }
-    return this.studentEntity.name
+    return this.studentEntity.name;
   }
- 
-
-  
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // rejectionDescription!: string;
 
-  // recuseReasons: any = {
-  //   NOT_ALLOWED_BACKGROUND: false,
-  //   COVERED_FACE: false,
-  //   NO_PERSON_RECOGNIZED: false,
-  //   OTHER_REASONS: false
-  // };
+// recuseReasons: any = {
+//   NOT_ALLOWED_BACKGROUND: false,
+//   COVERED_FACE: false,
+//   NO_PERSON_RECOGNIZED: false,
+//   OTHER_REASONS: false
+// };
 
-  // constructor(private updateSelfieService: UpdateSelfieStateService) {}
+// constructor(private updateSelfieService: UpdateSelfieStateService) {}
 
-  // GetSquareCoords() {
-  //   for (
-  //     let i = 0;
-  //     i < this.selfieToDisplay.automaticReview?.labels.length!;
-  //     i++
-  //   ) {
-  //     let label = this.selfieToDisplay.automaticReview?.labels[i];
+// GetSquareCoords() {
+//   for (
+//     let i = 0;
+//     i < this.selfieToDisplay.automaticReview?.labels.length!;
+//     i++
+//   ) {
+//     let label = this.selfieToDisplay.automaticReview?.labels[i];
 
-  //     let isAbleToDrawSquare: boolean =
-  //       (AutomaticReview.RejectedLabelslist.includes(label['name']) ||
-  //         label['parents'].some((parent: any) =>
-  //           AutomaticReview.RejectedLabelslist.includes(parent)
-  //         )) &&
-  //       Object.keys(label['coords']).length > 0;
+//     let isAbleToDrawSquare: boolean =
+//       (AutomaticReview.RejectedLabelslist.includes(label['name']) ||
+//         label['parents'].some((parent: any) =>
+//           AutomaticReview.RejectedLabelslist.includes(parent)
+//         )) &&
+//       Object.keys(label['coords']).length > 0;
 
-  //     if (isAbleToDrawSquare) {
-  //       let coordsObj = label['coords'];
-  //       return {
-  //         height: `${coordsObj['Height'] * 100}%`,
-  //         width: `${coordsObj['Width'] * 100}%`,
-  //         top: `${coordsObj['Top'] * 100}%`,
-  //         left: `${coordsObj['Left'] * 100}%`,
-  //       };
-  //     }
-  //   }
-  //   return null;
-  // }
+//     if (isAbleToDrawSquare) {
+//       let coordsObj = label['coords'];
+//       return {
+//         height: `${coordsObj['Height'] * 100}%`,
+//         width: `${coordsObj['Width'] * 100}%`,
+//         top: `${coordsObj['Top'] * 100}%`,
+//         left: `${coordsObj['Left'] * 100}%`,
+//       };
+//     }
+//   }
+//   return null;
+// }
 
-  // SetSelfieState(newState: string): void {
-  //   let newRecuseReasons: string[] = [];
+// SetSelfieState(newState: string): void {
+//   let newRecuseReasons: string[] = [];
 
-  //   for (let reason in this.recuseReasons) {
-  //     if (this.recuseReasons[reason]) {
-  //       newRecuseReasons.push(reason);
-  //     }
-  //   }
-  //   console.log(this.recuseReasons);
-  //   this.updateSelfieService
-  //     .UpdateSelfieState(
-  //       this.selfieToDisplay.student!.ra,
-  //       this.selfieToDisplay.idSelfie.toString(),
-  //       newState,
-  //       newRecuseReasons,
-  //       this.rejectionDescription
-  //     )
-  //     .subscribe((resp) => {
-  //       console.log(newRecuseReasons);
-  //       console.log(this.rejectionDescription);
+//   for (let reason in this.recuseReasons) {
+//     if (this.recuseReasons[reason]) {
+//       newRecuseReasons.push(reason);
+//     }
+//   }
+//   console.log(this.recuseReasons);
+//   this.updateSelfieService
+//     .UpdateSelfieState(
+//       this.selfieToDisplay.student!.ra,
+//       this.selfieToDisplay.idSelfie.toString(),
+//       newState,
+//       newRecuseReasons,
+//       this.rejectionDescription
+//     )
+//     .subscribe((resp) => {
+//       console.log(newRecuseReasons);
+//       console.log(this.rejectionDescription);
 
-  //       console.log(resp);
-  //       this.OnSetSelfieStateEvent.emit();
-  //     });
-  // }
+//       console.log(resp);
+//       this.OnSetSelfieStateEvent.emit();
+//     });
+// }
 
- 
-
-  // setReproveReason(reason: string) {
-  //   this.recuseReasons[reason] = !this.recuseReasons[reason];
-  // }
+// setReproveReason(reason: string) {
+//   this.recuseReasons[reason] = !this.recuseReasons[reason];
+// }
